edebaf20c3aa8aa6161adae053689f2fefff4442
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-03-02 13:23:55 -0500

    Document the relay_status values

--- sql/install.sql
+++ sql/install.sql
@@ -6,7 +6,7 @@ CREATE TABLE civicrm_sparkpost_router (
   `type` varchar(16) COLLATE utf8_unicode_ci NOT NULL COMMENT 'delivery, injection, etc.',
   `received_date` datetime DEFAULT NULL COMMENT 'When the webhook event was received',
   `relay_date` datetime DEFAULT NULL COMMENT 'Date when the message was relayed',
-  `relay_status` int(2) unsigned NOT NULL DEFAULT 0 COMMENT 'Whether the message has been relayed yet',
+  `relay_status` int(2) unsigned NOT NULL DEFAULT 0 COMMENT 'Relay status: 0=pending, 1=delivery, 2=error, 3=ignored',
   `customer_id` int(10) unsigned NOT NULL COMMENT 'SparkPost customer ID',
   `subaccount_id` int(10) unsigned NOT NULL COMMENT 'SparkPost subaccount ID',
   `message_id` varchar(32) DEFAULT NULL COMMENT 'SparkPost message ID',
